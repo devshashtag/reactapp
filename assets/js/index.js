b974b6fa157df5a8f4dfbc285c7f8dc1cabdd813
kamal heydari <kamal3heydari@gmail.com>
2021-12-17 21:07:34 +0330

    courses renderd

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
 //? global imports
+import { getElement, dispalyCourses } from "./utils.js";
 
 //? specific imports
+import courses from "./data/courses.js";
+
+//* get content elements
+const fullCourses = getElement(".full__content");
+const proposalCourses = getElement(".proposal__content");
+
+//* display items when page load
+window.addEventListener("DOMContentLoaded", function () {
+  fullCourses.innerHTML = dispalyCourses(courses, "full");
+  proposalCourses.innerHTML = dispalyCourses(courses, "proposal");
+});
