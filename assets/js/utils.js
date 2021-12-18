9b573e1b3306928d9eaf0029460d1f5ea9fb4c9b
kamal heydari <kamal3heydari@gmail.com>
2021-12-18 12:00:25 +0330

    render completed

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -8,39 +8,15 @@ const getElement = (selection) => {
   );
 };
 
-const dispalyCourses = (data = [], type = "title") => {
-  const filterItems = data.filter((item) => item[type]);
-
-  const displayItems = filterItems
-    .map((item) => {
-      return ` <article class="course">
-<div class="course__header">
-  <img src=${item.image} alt=${item.title} />
-  <div class='header__icon' >
-  <i class="fas fa-play "></i>
-  <i class="fas fa-user  "></i>
-  </div>
-</div>
-<div class="course__content">
-  <h3 class="content__title">${item.title}</h3>
-  <div class="content__author">
-  <i class="fas fa-chalkboard-teacher"></i>
-  <span>${item.author}</span></div>
-</div>
-<div class="course__footer">
-<div class="footer__student">
-<i class="fas fa-users"></i>
-<span>${item.students}</span>
-</div>
-  <div class="footer__price">
-  <span>${item.price}</span>
-  </div>
-</div>
-</article>`;
-    })
-    .join("");
-
-  return displayItems;
+const separate = (Number) => {
+  Number += "";
+  Number = Number.replace(",", "");
+  let x = Number.split(".");
+  let y = x[0];
+  let z = x.length > 1 ? "." + x[1] : "";
+  let rgx = /(\d+)(\d{3})/;
+  while (rgx.test(y)) y = y.replace(rgx, "$1" + "," + "$2");
+  return y + z;
 };
 
-export { getElement, dispalyCourses };
+export { getElement, separate };
