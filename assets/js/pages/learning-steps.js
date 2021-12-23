f6991e3d278dab46814eeb785ebb19db64cbeb71
devs.hashtag <devs.hashtag@gmail.com>
2021-12-23 05:48:01 +0330

    pages/learning-steps header added

--- assets/js/pages/learning-steps.js
+++ assets/js/pages/learning-steps.js
@@ -1,3 +1,12 @@
 //? global imports
+import { headerInit } from "/assets/js/global/header.js";
 
 //? specific imports
+
+
+
+window.addEventListener("DOMContentLoaded", function () {
+
+  headerInit(); // header
+
+});
