c6ce3bc94353214169f4c937cf570c252f3bf9b2
devs.hashtag <devs.hashtag@gmail.com>
2021-12-23 06:49:47 +0330

    pages/my-account header added

--- assets/js/pages/my-account.js
+++ assets/js/pages/my-account.js
@@ -1,3 +1,13 @@
 //? global imports
+//? global imports
+import { headerInit } from "/assets/js/global/header.js";
 
 //? specific imports
+
+
+window.addEventListener("DOMContentLoaded", function () {
+
+  headerInit(); // header
+
+});
+
