c330da5b1f0b9da57a21824cfdfced91f1654207
devs.hashtag <devs.hashtag@gmail.com>
2021-12-25 07:41:26 +0330

    header added to lost-password header added to pages/my-account/lost-password

--- assets/js/pages/my-account/lost-password.js
+++ assets/js/pages/my-account/lost-password.js
@@ -1,10 +1,12 @@
 //* ReactApp
 //* for: ReactApp/pages/my-account/lost-password.html
+import { headerInit } from "/assets/js/global/header.js";
 
-// import {  } from "";
 
 //* display items when page load
 window.addEventListener("DOMContentLoaded", function () {
-  
+
+  headerInit(); // header
+
 });
 
