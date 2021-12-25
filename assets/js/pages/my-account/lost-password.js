edc791f221add6932e05aec142dcc67a11cc0d27
devs.hashtag <devs.hashtag@gmail.com>
2021-12-25 07:35:30 +0330

    lost-password script and scss files created

---
+++ assets/js/pages/my-account/lost-password.js
@@ -0,0 +1,10 @@
+//* ReactApp
+//* for: ReactApp/pages/my-account/lost-password.html
+
+// import {  } from "";
+
+//* display items when page load
+window.addEventListener("DOMContentLoaded", function () {
+  
+});
+
