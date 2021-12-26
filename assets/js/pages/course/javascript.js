6603761edc6932cd810ad20f08b932fc2d69186a
devs.hashtag <devs.hashtag@gmail.com>
2021-12-26 19:43:08 +0330

    fix header conflict with assets/js/pages/course/javascript

--- assets/js/pages/course/javascript.js
+++ assets/js/pages/course/javascript.js
@@ -3,82 +3,82 @@ import { headerInit } from "/assets/js/global/header.js"
 
 let $ = document;
 
-// click on btn-up-page
-const btnUpPage = getElement('.btn-scroll');
+//* display items when page load
+window.addEventListener("DOMContentLoaded", function () {
 
-window.addEventListener('scroll', () => {
-  // scroll-to-top
-  if (window.scrollY > 60) {
-    btnUpPage.classList.add('btn-scroll-active');
-  } else {
-    btnUpPage.classList.remove('btn-scroll-active');
-  }
-  
-});
+  // header
+  headerInit();
 
-// Frequently Asked Questions
-const questions = getElement(".frequently-questions");
+  window.addEventListener('scroll', () => {
+    // click on btn-up-page
+    const btnUpPage = getElement('.btn-scroll');
 
-questions.addEventListener('click', (el) => {
-  let elmParent = el.target.parentNode;
+    // scroll-to-top
+    if (window.scrollY > 60) {
+      btnUpPage.classList.add('btn-scroll-active');
+    } else {
+      btnUpPage.classList.remove('btn-scroll-active');
+    }
 
-  if(el.target.classList.contains("question"))
-    elmParent.classList.toggle('questions-active');
+  });
 
-  if(elmParent.classList.contains("question"))
-    elmParent.parentElement.classList.toggle('questions-active');
-});
+  // Frequently Asked Questions
+  const questions = getElement(".frequently-questions");
 
-// Seasons and Projects
+  questions.addEventListener('click', (el) => {
+    let elmParent = el.target.parentNode;
 
-const seasonsTitles = $.querySelectorAll('.seasons-projects');
-const seasonInfo = $.querySelectorAll('.season-info');
+    if(el.target.classList.contains("question"))
+      elmParent.classList.toggle('questions-active');
 
-seasonsTitles.forEach(seasonsTitle => {
+    if(elmParent.classList.contains("question"))
+      elmParent.parentElement.classList.toggle('questions-active');
+  });
+
+  // Seasons and Projects
+  const seasonsTitles = $.querySelectorAll('.seasons-projects');
+  const seasonInfo = $.querySelectorAll('.season-info');
+
+  seasonsTitles.forEach(seasonsTitle => {
     seasonsTitle.addEventListener('click', () => {
-        seasonsTitle.parentElement.classList.toggle('seasons-projects-box-active');
-        seasonsTitle.lastElementChild.classList.toggle('down-icon-active');
-        console.log(seasonsTitle)
+      seasonsTitle.parentElement.classList.toggle('seasons-projects-box-active');
+      seasonsTitle.lastElementChild.classList.toggle('down-icon-active');
+      console.log(seasonsTitle)
 
-        seasonInfo.forEach(season_info => {
-            if (seasonsTitle.dataset.number == season_info.dataset.number) {
-                season_info.classList.toggle('season-info-active');
-            }
-        });
+      seasonInfo.forEach(season_info => {
+        if (seasonsTitle.dataset.number == season_info.dataset.number) {
+          season_info.classList.toggle('season-info-active');
+        }
+      });
     });
-});
+  });
 
-// scroll window and click on studyMode
-const asideCourse = $.querySelector('.aside-course');
-const mains = $.querySelectorAll('main');
-const studyMode = $.querySelector('.study-mode');
+  // scroll window and click on studyMode
+  const asideCourse = $.querySelector('.aside-course');
+  const mains = $.querySelectorAll('main');
+  const studyMode = $.querySelector('.study-mode');
 
-studyMode.addEventListener('click', () => {
+  studyMode.addEventListener('click', () => {
     asideCourse.classList.toggle('aside-study-mode');
     mains.forEach(main => {
-        main.classList.toggle('main-active');
+      main.classList.toggle('main-active');
     })
-})
+  })
 
-// aside and study-mode fixed
-window.addEventListener('scroll', () => {
+  // aside and study-mode fixed
+  window.addEventListener('scroll', () => {
     if (window.scrollY >= 600) {
-        studyMode.classList.add('study-mode-active');
+      studyMode.classList.add('study-mode-active');
     } else {
-        studyMode.classList.remove('study-mode-active');
+      studyMode.classList.remove('study-mode-active');
     }
 
     if (window.scrollY >= 1050) {
-        asideCourse.classList.add('aside-active');
+      asideCourse.classList.add('aside-active');
     } else {
-        asideCourse.classList.remove('aside-active');
+      asideCourse.classList.remove('aside-active');
     }
-});
+  });
 
-
-//* display items when page load
-window.addEventListener("DOMContentLoaded", function () {
-  // header
-  headerInit();
 });
 
