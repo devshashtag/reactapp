373d3c755abad4f917dc44d2e568eedadcbab68d
kamal heydari <kamal3heydari@gmail.com>
2021-12-29 21:22:36 +0330

    /global/display change class names & wrap img,author and title between a tag

--- assets/js/global/display.js
+++ assets/js/global/display.js
@@ -5,21 +5,28 @@ const dispalyCourses = (data = [], type = "title") => {
 
   const displayItems = filterItems
     .map((item) => {
-      return ` <article class="course">
-  <div class="course__header">
+      return ` <article class="course-card">
+  <div class="course-card__header">
+    <a href=${item.url ? item.url : "#"}>
     <img src='${item.image}' alt='${item.title}' />
+    </a>
     <div class='header__icon' >
     <i class="fas fa-play"></i>
     <i class="fas fa-user-plus"></i>
     </div>
   </div>
-  <div class="course__content">
-    <h3 class="content__title">${item.title}</h3>
+  <div class="course-card__content">
+    <h3 class="content__title">
+    <a href=${item.url ? item.url : "#"}>
+    ${item.title}
+    </a>
+    </h3>
     <div class="content__author">
     <i class="fas fa-chalkboard-teacher"></i>
-    <span>${item.author}</span></div>
+    <a href='#'>${item.author}</a>
+    </div>
   </div>
-  <div class="course__footer">
+  <div class="course-card__footer">
   <div class="footer__student">
   <i class="fas fa-users"></i>
   <span>${separate(item.students)}</span>
@@ -39,16 +46,20 @@ const displayBlogs = (data = [], value) => {
   const displayItmes = data
     .slice(0, value)
     .map((item) => {
-      return `<article class="blog">
+      return `<article class="blog-card">
+  <a href=${item.url ? item.url : "#"}>
   <img
     src="${item.image}"
     alt="${item.name}"
-    class="blog__img"
-  />
-  <h3 class="blog__title">
+    class="blog-card__img"
+     />
+  </a>
+  <h3 class="blog-card__title">
+  <a href=${item.url ? item.url : "#"}>
   ${item.title}
+  </a>
   </h3>
-  <button class="blog__btn">بیشتر بخوانید</button>
+  <a href=${item.url ? item.url : "#"} class="blog-card__btn">بیشتر بخوانید</a>
 </article>`;
     })
     .join("");
