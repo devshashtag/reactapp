b1c583a64bde43843c6664558a642b1b7bc7d0be
devs.hashtag <devs.hashtag@gmail.com>
2022-01-10 15:21:19 +0330

    blogs footer added
    blogs display issue fixed

--- assets/js/pages/blog.js
+++ assets/js/pages/blog.js
@@ -1,24 +1,17 @@
-//? global imports
 import { getElement } from "/assets/js/global/utils.js";
 import { displayBlogs } from "/assets/js/global/display.js";
 
-import { headerInit } from "/assets/js/global/header.js";
+import { renderDefaultTemplate } from "/assets/js/global/render.js";
 
-//? specific imports
+// blogs data
 import blogs from "/assets/js/data/blogs.js";
 
-//* get content element
-const blogsContent = getElement(".blogs__content");
-
-//* display items when page load
+//* runs when page load
 window.addEventListener("DOMContentLoaded", function () {
-  displayAllBlogs();
-
-  headerInit(); // header
-});
-
-//* functions
-const displayAllBlogs = () => {
+  //* get content element
+  const blogsContent = getElement(".blogs__content");
   blogsContent.innerHTML = displayBlogs(blogs);
-};
 
+  // header, footer
+  renderDefaultTemplate(document.body.innerHTML);
+});
