6b2d61a21fdbe94b18ff55e0e8d7b1324afc7d1e
devs.hashtag <devs.hashtag@gmail.com>
2022-05-03 15:04:01 +0430

    learning-steps page topbar added

--- assets/js/pages/learning-steps.js
+++ assets/js/pages/learning-steps.js
@@ -1,9 +1,11 @@
-import { navbarEvent, sidebarEvent } from "/assets/js/global/headerEvents.js";
+import { topbarEvent, navbarEvent, sidebarEvent } from "/assets/js/global/headerEvents.js";
 
 window.addEventListener("DOMContentLoaded", function () {
   // code here
 
   // Header Events
+  topbarEvent();
   navbarEvent();
   sidebarEvent();
 });
+
