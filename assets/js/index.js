5a38423ba2aeed052f16dcb6944c1be67bc9730e
devs.hashtag <devs.hashtag@gmail.com>
2022-05-03 14:24:56 +0430

    index.js import headerEvents in one line

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,7 @@
 //? global imports
 import { getElement } from "/assets/js/global/utils.js";
 import { dispalyCourses, displayBlogs } from "/assets/js/global/display.js";
-import {
-  topbarEvent,
-  navbarEvent,
-  sidebarEvent,
-} from "/assets/js/global/headerEvents.js";
+import { topbarEvent, navbarEvent, sidebarEvent } from "/assets/js/global/headerEvents.js";
 
 //? specific imports
 import courses from "/assets/js/data/courses.js";
