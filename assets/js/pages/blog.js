2543a79866a9d4fa41b3a91bac75e92695b0df14
devs.hashtag <devs.hashtag@gmail.com>
2022-05-03 14:25:19 +0430

    blog page headerEvents added

--- assets/js/pages/blog.js
+++ assets/js/pages/blog.js
@@ -1,18 +1,19 @@
 import { getElement } from "/assets/js/global/utils.js";
 import { displayBlogs } from "/assets/js/global/display.js";
-
-import { renderDefaultTemplate } from "/assets/js/global/render.js";
+import { topbarEvent, navbarEvent, sidebarEvent } from "/assets/js/global/headerEvents.js";
 
 // blogs data
 import blogs from "/assets/js/data/blogs.js";
 
 //* runs when page load
 window.addEventListener("DOMContentLoaded", function () {
-  // header, footer
-  renderDefaultTemplate(document.body.innerHTML);
-
   // code here
   //* get content element
   const blogsContent = getElement(".blogs__content");
   blogsContent.innerHTML = displayBlogs(blogs);
+
+  // Header Events
+  topbarEvent();
+  navbarEvent();
+  sidebarEvent();
 });
